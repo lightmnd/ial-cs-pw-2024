60601f5bb045ffa3daa4521a1f9380e19221e115
lightmnd <booking.djonly@gmail.com>
2024-09-09 12:59:16 +0200

    dbconn modified with correct params

--- DBConn.php
+++ DBConn.php
@@ -1,15 +1,26 @@
 <?php
-$hostname_DBConn = "localhost"; // link connessione
-$database_DBConn = "sito_test_db"; // Nome database
-$username_DBConn = "sito_test_user"; // Utente database
-$password_DBConn = "Margot2024!!"; // pw dr
+$hostname_DBConn = "localhost";
+$database_DBConn = "test";
+$username_DBConn = "root";
+$password_DBConn = "Margot2024!!";
 
 $DBConn = mysqli_connect($hostname_DBConn, $username_DBConn, $password_DBConn, $database_DBConn);
 mysqli_set_charset($DBConn, "utf8");
-if (!$DBConn) {
-	echo "Error: Unable to connect to MySQL." . PHP_EOL;
-	echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-	echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-	exit;
+
+if (mysqli_connect_errno()) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}   
+
+$query = "SELECT VERSION()";
+if ($result = mysqli_query($DBConn, $query)) {
+    $row = mysqli_fetch_assoc($result);
+    // echo "Connected to database. MySQL version: " . $row['VERSION()'];
+    mysqli_free_result($result);
+} else {
+    echo "Error executing query: " . mysqli_error($DBConn);
 }
+
 $GLOBALS['DBConn'] = $DBConn;
